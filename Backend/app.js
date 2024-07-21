45e9e7ab29ef441398fdd945678ec2e27b446361
Oscar Frias Viñals <140022816+ofvinals@users.noreply.github.com>
2024-07-21 17:22:08 -0300

    configuro cors para deploy

--- Backend/app.js
+++ Backend/app.js
@@ -33,15 +33,15 @@ app.use(
 	})
 );
 
-// app.use((req, res, next) => {
-//   res.setHeader(
-//     'Access-Control-Allow-Origin',
-//     'https://proyecto-estudio-mongo.vercel.app'
-//   );
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   next();
-// });
+app.use((req, res, next) => {
+	res.setHeader(
+		'Access-Control-Allow-Origin',
+		'https://resto-flow-gestion-gastronomica.vercel.app'
+	);
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+	next();
+});
 
 app.use(express.json());
 
