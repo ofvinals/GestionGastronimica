ff680e6656165581783fae5a60048c32731aeabb
Oscar Frias Viñals <140022816+ofvinals@users.noreply.github.com>
2024-07-22 13:34:01 -0300

    corrijo loader

--- Frontend/src/helpers/Loader.jsx
+++ Frontend/src/helpers/Loader.jsx
@@ -2,7 +2,7 @@ import '../css/Custom.css';
 
 const Loader = () => {
   return (
-    <div className="loader-page">
+    <div className="loader-page text-slate-700">
       Cargando...
     </div>
   );
